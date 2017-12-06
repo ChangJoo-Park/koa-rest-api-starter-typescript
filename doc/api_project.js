5edfe7618a75c0895f6b6f1c93281e43cc825c30
ChangJoo Park <pcjpcj2@gmail.com>
2017-12-06 21:54:02 +0900

    First api doc

---
+++ doc/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "api doc",
+  "version": "0.1.0",
+  "description": "A basic apiDoc",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-12-06T12:52:17.294Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
